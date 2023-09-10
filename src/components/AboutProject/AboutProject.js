15c23be79fc4b558e67ed4d7df17608cc1ed0410
AnnaAslanian <anyutka069@yandex.ru>
2023-09-10 21:44:43 +0300

    4-ревью

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -5,12 +5,12 @@ function AboutProject() {
     return (
         <section className="about-project" id="about">
             <h2 className="about-project__title">О проекте</h2>
-            <ul className="about-project__block">
-                <li className="about-project_block-info">
+            <ul className="about-project__blocks">
+                <li className="about-project__block">
                 <h3 className="about-project__card-title">Дипломный проект включал 5 этапов</h3>
                 <p className="about-project__card-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
             </li>
-            <li className="about-project_block-info">
+            <li className="about-project__block">
                 <h3 className="about-project__card-title">На выполнение диплома ушло 5 недель</h3>
                 <p className="about-project__card-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
             </li>
